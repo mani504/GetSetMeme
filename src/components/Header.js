529f83344c1dba8b22b48316d29e0d445ed3f4fc
Manish <manishraj3322@gmail.com>
2022-06-30 15:39:24 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="https://tinyurl.com/yc5xyzbb" alt="logo" className="header--image" />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">How does the ocean say hi? It waves!</h4>
+        </header>
+    )
+}
